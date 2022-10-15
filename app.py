6102a83789068871ec6cc2c9f0425cdf32209b77
FackOff25 <kozvladimir@bk.ru>
2022-10-15 11:36:17 +0300

    working

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, url_for, render_template, request, json, session, redirect
 from auth.route import blueprint_auth
 from blueprint_query.route import blueprint_query
-from access import login_required
+# from access import login_required
 
 app = Flask(__name__)
 app.secret_key = 'Superley'
@@ -10,7 +10,7 @@ app.register_blueprint(blueprint_auth, url_prefix='/auth')
 app.register_blueprint(blueprint_query, url_prefix='/requests')
 
 app.config['db_config'] = json.load(open('data_files/dbconfig.json'))
-app.config['access_config'] = json.load(open('data_files/access.json'))
+# app.config['access_config'] = json.load(open('data_files/access.json'))
 
 
 @app.route('/', methods=['GET', 'POST'])
